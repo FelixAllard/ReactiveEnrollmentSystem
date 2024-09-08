c553de2a0811bed73d149c27dace0bddf7accb4e
Felix Allard <xilef992@gmail.com>
2024-09-08 19:32:04 -0400

    Get all CourseControllerIntegrationTest added  getAllCourses_whenNoCourses_shouldReturnEmptyStream()

--- courses-service/src/test/java/com/champlain/courseservice/presentationlayer/ControllerIntegrationTest.java
+++ courses-service/src/test/java/com/champlain/courseservice/presentationlayer/ControllerIntegrationTest.java
@@ -1,21 +1,35 @@
 package com.champlain.courseservice.presentationlayer;
 
+import com.champlain.courseservice.businesslayer.CourseService;
+import com.champlain.courseservice.businesslayer.CourseServiceImpl;
 import com.champlain.courseservice.dataaccesslayer.CourseRepository;
+import com.champlain.courseservice.utils.exceptions.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static java.lang.Math.log;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+@Slf4j
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -25,7 +39,7 @@ class CourseControllerIntegrationTest {
     private WebTestClient webTestClient;
     @Autowired
     private CourseRepository courseRepository;
-    private String validCourseId;
+    private final String validCourseId = "77918ba2-49da-4c67-bea8-9f26e5d355fb";
     private final Long dbSize = 1000L;
 
     @BeforeEach
@@ -86,6 +100,62 @@ class CourseControllerIntegrationTest {
                     assertNotNull(list);
                     assertEquals(dbSize, list.size());
                 });
+    }
+    @Test
+    void getCourseByCourseId_shouldSucceedWithExistingId() {
+        webTestClient
+                .get()
+                .uri("/api/v1/courses/" + validCourseId)
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-Type", "text/event-stream;charset=UTF-8")
+                .expectBodyList(CourseResponseModel.class)
+                .value(list -> {
+                    assertNotNull(list);
+                    assertEquals(1, list.size());
+                    assertEquals(validCourseId, list.get(0).getCourseId());
+                });
+    }
+
+    @Test
+    void getCourseByCourseId_shouldReturnNotFound_WithNonExistingId() {
+        webTestClient
+                .get()
+                .uri("/api/v1/courses/" + "77918ba2-49da-4c67-bea8-111111111111")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectHeader().valueEquals("Content-Type", "text/event-stream;charset=UTF-8")
+                .expectBodyList(CourseResponseModel.class);
+    }
+    @Test
+    void getCourseByCourseId_shouldReturnUnProcessableEntity_WithInvalidId() {
+        webTestClient
+                .get()
+                .uri("/api/v1/courses/" + "77918ba2-49da-4c67-bea8-111")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isEqualTo(422) // Unprocessable Entity
+                .expectBody(String.class);
+    }
+    @Test
+    void getAllCourses_whenNoCourses_shouldReturnEmptyStream() {
+        courseRepository.deleteAll().block(); // Add block so the process won't be to fast
+        webTestClient
+                .get()
+                .uri("/api/v1/courses")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-Type", "text/event-stream;charset=UTF-8")
+                .expectBodyList(CourseResponseModel.class)
+                .value((list) -> {
+                    assertNotNull(list);
+                    assertEquals(courseRepository.count().block(), list.size()); // Also block here cause else we get a problem
+                    assertEquals(list.size(), 0);
+                });
+
     }
 
 }
